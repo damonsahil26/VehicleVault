97f68ad0c60c25d65bbc0279a7d8e409000f1d93
Sahil Malhotra <sahil061626@gmail.com>
2023-12-02 18:36:43 +0530

    Added placeholder data

--- VehicleVault.Maui/Services/CarService.cs
+++ VehicleVault.Maui/Services/CarService.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VehicleVault.Maui.Models;
 
 namespace VehicleVault.Maui.Services
 {
     public class CarService
     {
+        public List<Car> GetCars()
+        {
+            return [
+                new() { Id = 1, Brand = "Tata", Model = "Nano", Vin = "ABC123" },
+                new() { Id = 2, Brand = "Maruti Suzuki", Model = "Nexon", Vin = "MARUTI123" },
+                new() { Id = 3, Brand = "BMW", Model = "M3", Vin = "BMW123" },
+                new() { Id = 4, Brand = "Audi", Model = "A5", Vin = "AUDI123" },
+                new() { Id = 5, Brand = "Nissan", Model = "Sunny", Vin = "NISSAN123" },
+                new() { Id = 6, Brand = "Ferrari", Model = "Spider", Vin = "FER123" },
+            ];
+        }
     }
 }
